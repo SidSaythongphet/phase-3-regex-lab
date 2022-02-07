1c0c2e8513bd1b8b3763cff1159e68fa39c94ebe
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-06 19:52:18 -0500

    commit tree f296cd4579d70d5bf5ea8f877b879596464bc91e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/\b[aeiou]/) then return true
+    else return false
+    end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
+puts starts_with_a_vowel? 'upstairs'
 
+puts starts_with_a_vowel? 'bubbles'
+
+def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\bun+\b.*ing/).length
 end
 
 def words_five_letters_long(text)
